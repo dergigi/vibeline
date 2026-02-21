a62ab3d4925be4a012b284a0eca9e1b0255c1b5f
Gigi <dergigi@pm.me>
2026-02-21 07:52:42 +0100

    fix: pass DM_NSEC to nak publish to avoid using NOSTR_SECRET_KEY

--- scripts/dm.sh
+++ scripts/dm.sh
@@ -98,6 +98,6 @@ fi
 # 3. Publish the gift wrap to DM relays
 nak event -k 14 -c "$TRANSCRIPT" --tag p="$RECIPIENT_HEX" |
     nak gift wrap --sec "$DM_NSEC" -p "$MATCHED_NPUB" |
-    nak publish ${RELAYS}
+    nak publish --sec "$DM_NSEC" ${RELAYS}
 
 echo "NIP-17 DM sent to $HEY_NAME ($MATCHED_NPUB)" >&2
